bc93f07830b7a61f4f83d5186db5f2357514a481
Jacob Tompkins <term171013@gmail.com>
2018-05-10 13:07:40 +0800

    almost finished models, need to correct raise_with_error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+
+Ingredient.destroy_all
+puts "Ingredients destroyed"
+
+drinks_json = File.read('/home/jake/code/JakeTompkins/rails-mister-cocktail/db/ingredients.json')
+json = JSON.parse(drinks_json)
+drinks = json["drinks"]
+drinks.each do |d|
+  Ingredient.create(name:d.values[0])
+  puts "#{Ingredient.last.name} created"
+end
+
+puts "created #{Ingredient.all.count} ingredients"
